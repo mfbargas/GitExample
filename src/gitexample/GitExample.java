943021145e67274458e9adc1764af2e0fac8e3cc
Manuel Fragoso <fragoso.manuel@gmail.com>
2019-01-02 17:34:27 -0600

    Version inicial del proyecto

---
+++ src/gitexample/GitExample.java
@@ -0,0 +1,11 @@
+package gitexample;
+
+public class GitExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("SELECT * from invoices where invoice_amount > 0");
+
+	}
+
+}
