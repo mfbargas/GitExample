f4c45686a78858d1f68c2b785f15ceeefd11ec82
Manuel Fragoso <fragoso.manuel@gmail.com>
2019-01-02 17:39:21 -0600

    Version 2, metimos un filtro de no canceladas

--- src/gitexample/GitExample.java
+++ src/gitexample/GitExample.java
@@ -5,6 +5,9 @@ public class GitExample {
 	public static void main(String[] args) {
 		
 		System.out.println("SELECT * from invoices where invoice_amount > 0");
+		
+		System.out.println("where invoice_status not in (Cancelled)");
+		
 
 	}
 
